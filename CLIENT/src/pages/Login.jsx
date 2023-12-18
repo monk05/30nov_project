7d2057db475b266ff0aac7db2c93d3c41eea79d0
monk05 <itsmespandey18@gmail.com>
2023-12-18 20:52:17 +0530

    hooks 1st

--- CLIENT/src/pages/Login.jsx
+++ CLIENT/src/pages/Login.jsx
@@ -1,3 +1,69 @@
+import { useState } from "react"
+
 export const Login=()=>{
-    return <h1>Login page </h1>
+ const[user,setUser]=useState({
+    email:"",
+    password:""
+ });
+
+ const handleInput=(e)=>{
+    let name=e.target.name;
+    let value=e.target.value;
+
+    setUser({
+        ...user,
+        [name]:value
+    })
+ }
+
+ const handleSubmit=(e)=>{
+    e.preventDefault();
+ }
+
+
+
+
+    return <>
+        <section>
+            <div>
+                <div className="image">
+
+                </div>
+                <div className="login-form">
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="email">email :</label>
+                            <input 
+                                type="text"
+                                name="email"
+                                placeholder="email"
+                                id="email"
+                                required
+                                autoComplete="off"
+                                value={user.email}
+                                onChange={handleInput}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">password :</label>
+                            <input 
+                                type="password"
+                                name="password"
+                                placeholder="password"
+                                id="password"
+                                required
+                                autoComplete="off"
+                                value={user.password}
+                                onChange={handleInput}
+                            />
+                    
+
+                        </div>
+
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </>
 }
--- CLIENT/src/pages/Register.jsx
+++ CLIENT/src/pages/Register.jsx
@@ -1,3 +1,119 @@
+import { useState } from "react"
+
 export const Register=()=>{
-    return <h1>Register Page</h1>
+    const [user,setUser]=useState({ //creating objects 
+        username:"",
+        email:"",
+        phone:"",
+        password:""
+    });
+
+    //handling the input
+
+    const handleInput=(e)=>{
+        let name=e.target.name;  //gets name of the field 
+        let value=e.target.value;  // gets entered value in the field live
+
+        setUser({
+            ...user, // SPREAD OPERATOR (only modify the modified field rest keep it default)
+            [name]:value, //dynamic 
+
+        })
+    }
+
+    //handling the form submit
+    const  handleSubmit=(e)=>{
+        e.preventDefault(); // prevent reload
+        
+    }
+    
+
+
+
+    return <>
+        <section>
+            <main>
+                <div className="section-registration">
+                    <div className="container grid grid-two-cols">
+                        {/*image on reg. page*/}
+                        <div className="registration-image">
+                            <img src="/images/register.png" 
+                            alt="registration form "
+                            width="500"
+                            height="500"
+                             />
+                        </div>
+                        {/*registration form */}
+                        <div className="registration-form">
+                            <h1 className="main-heading mb-3">registration form</h1>
+                            <br/>
+                            <form onSubmit={handleSubmit} >
+                                <div>
+                                    <label htmlFor="username">username :</label>
+                                    <input  
+                                        type="text" 
+                                        name="username" 
+                                        placeholder="username"
+                                        id="username"
+                                        required
+                                        autoComplete="off"
+                                        value={user.username}
+                                        onChange={handleInput}
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="email">email :</label>
+                                    <input  
+                                        type="text" 
+                                        name="email" 
+                                        placeholder="email"
+                                        id="email"
+                                        required
+                                        autoComplete="off"
+                                        value={user.email}
+                                        onChange={handleInput}
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="phone">phone : </label>
+                                    <input 
+                                        type="text" 
+                                        name="phone" 
+                                        placeholder="phone"
+                                        id="phone"
+                                        required
+                                        autoComplete="off"
+                                        value={user.phone}
+                                        onChange={handleInput}
+
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="password">password :</label>
+                                    <input 
+                                        type="password" 
+                                        name="password" 
+                                        placeholder="password"
+                                        id="password"
+                                        required
+                                        autoComplete="off"
+                                        value={user.password}
+                                        onChange={handleInput}
+                                    />
+                                </div>
+                                <br/>
+                                <button type="submit" className="btn btn-submit">Register now</button>
+
+                            </form>
+
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+            </main>
+        </section>
+    </>
 }
